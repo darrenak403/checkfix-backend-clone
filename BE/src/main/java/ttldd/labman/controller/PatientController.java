a29f71f3a6da590e14834c25d93fa3f52dd31e8f
Lương Gia Lâm <laml33366@gmail.com>
2025-09-24 15:15:38 +0000

    Resolve "Api add patient"

--- BE/src/main/java/ttldd/labman/controller/PatientController.java
+++ BE/src/main/java/ttldd/labman/controller/PatientController.java
@@ -4,7 +4,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ttldd.labman.dto.PatientDTO;
+import ttldd.labman.dto.request.PatientRequest;
+import ttldd.labman.dto.response.PatientResponse;
+import ttldd.labman.dto.response.RestResponse;
 import ttldd.labman.service.PatientService;
 
 @RestController
@@ -15,14 +17,15 @@ public class PatientController {
     private final PatientService patientService;
 
     @PostMapping
-    public ResponseEntity<PatientDTO> createPatient(@Valid @RequestBody PatientDTO request) {
-        request = patientService.createPatient(request);
-        return ResponseEntity.ok(request);
+    public ResponseEntity<RestResponse<PatientResponse>> createPatient(@Valid @RequestBody PatientRequest request) {
+        PatientResponse res = patientService.createPatient(request);
+        RestResponse<PatientResponse> response = RestResponse.<PatientResponse>builder()
+                .statusCode(201)
+                .message("Patient created successfully")
+                .data(res)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/greeting")
-    public String greeting() {
-        return "Hello, welcome to the Patient Management System!";
-    }
 
 }
