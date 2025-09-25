2019e047d329957814cb2ce7de0f731d3ac69271
LamGia <laml33366@gmail.com>
2025-09-25 11:01:18 +0700

    add PatientServiceImp

---
+++ BE/src/main/java/ttldd/labman/service/imp/PatientServiceImp.java
@@ -0,0 +1,50 @@
+package ttldd.labman.service.imp;
+
+import ttldd.labman.dto.request.PatientRequest;
+import ttldd.labman.dto.response.PatientResponse;
+import ttldd.labman.entity.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ttldd.labman.repo.PatientRepo;
+import ttldd.labman.service.PatientService;
+
+@Service
+@RequiredArgsConstructor
+public class PatientServiceImp implements PatientService {
+
+    private final PatientRepo patientRepo;
+
+    @Override
+    public PatientResponse createPatient(PatientRequest patientDTO) {
+        Patient patient = Patient.builder()
+                .name(patientDTO.getFullName())
+                .yob(patientDTO.getYearOfBirth())
+                .gender(patientDTO.getGender())
+                .address(patientDTO.getAddress())
+                .phone(patientDTO.getPhone())
+                .email(patientDTO.getEmail())
+                .lastTestDate(patientDTO.getLastTestDate())
+                .lastTestType(patientDTO.getLastTestType())
+                .instrumentUsed(patientDTO.getInstrumentUsed())
+                .build();
+        patientRepo.save(patient);
+        return mapToResponse(patient);
+    }
+
+    private PatientResponse mapToResponse(Patient patient) {
+        PatientResponse response = new PatientResponse();
+        response.setId(patient.getId());
+        response.setName(patient.getName());
+        response.setYob(patient.getYob());
+        response.setGender(patient.getGender());
+        response.setAddress(patient.getAddress());
+        response.setPhone(patient.getPhone());
+        response.setEmail(patient.getEmail());
+        response.setLastTestDate(patient.getLastTestDate());
+        response.setLastTestType(patient.getLastTestType());
+        response.setInstrumentUsed(patient.getInstrumentUsed());
+        return response;
+    }
+
+
+}
