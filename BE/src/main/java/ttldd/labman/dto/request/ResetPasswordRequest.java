1e438d6f69d3e16c8714916afa9bc129e253c766
Nguyễn Thành Đạt <datnguyen.coder312@gmail.com>
2025-09-26 07:30:24 +0700

    fix/emailservices

--- BE/src/main/java/ttldd/labman/dto/request/ResetPasswordRequest.java
+++ BE/src/main/java/ttldd/labman/dto/request/ResetPasswordRequest.java
@@ -21,7 +21,7 @@ public class ResetPasswordRequest {
     private String otp;
     
     @NotBlank(message = "Mật khẩu mới không được để trống")
-    @Pattern(regexp = "^(^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,})$\n",
-             message = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và số ")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
+             message = "Mật khẩu phải có ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt")
     private String newPassword;
 }
