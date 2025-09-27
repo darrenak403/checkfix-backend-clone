b714a3a7798070c0204ddeb4cb21b432448aac56
Lương Gia Lâm <laml33366@gmail.com>
2025-09-27 05:00:56 +0000

    Resolve "Api deleted patient"

--- BE/src/main/java/ttldd/labman/service/PatientService.java
+++ BE/src/main/java/ttldd/labman/service/PatientService.java
@@ -9,5 +9,9 @@ public interface PatientService {
     public PatientResponse createPatient(PatientRequest patientDTO);
     public List<PatientResponse> getAllPatients();
     public PatientResponse updatePatient(Long id, PatientRequest patientDTO);
+<<<<<<< HEAD
+    public void deletePatient(Long id);
+=======
 
+>>>>>>> a46baebf7f0416d41011ef25bb7d5a36256d5562
 }
