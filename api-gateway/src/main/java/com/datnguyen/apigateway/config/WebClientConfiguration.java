c6fe027d511a15a4207d91c583c7c8693882f55b
LamGia <laml33366@gmail.com>
2025-10-11 01:16:14 +0700

    config url lb for services

--- api-gateway/src/main/java/com/datnguyen/apigateway/config/WebClientConfiguration.java
+++ api-gateway/src/main/java/com/datnguyen/apigateway/config/WebClientConfiguration.java
@@ -2,24 +2,60 @@ package com.datnguyen.apigateway.config;
 
 
 import com.datnguyen.apigateway.repository.IamClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Configuration
 public class WebClientConfiguration {
     @Bean
-    WebClient webClient(){
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8081/iam")
+                .filter(forwardAuthHeader());
+    }
+
+    @Bean
+    public ExchangeFilterFunction forwardAuthHeader() {
+        return (clientRequest, next) -> Mono.deferContextual(contextView ->
+                contextView.<ServerWebExchange>getOrEmpty(ServerWebExchange.class)
+                        .map(serverWebExchange -> {
+                            String token = serverWebExchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+                            if (token != null && !token.isEmpty()) {
+                                ClientRequest newRequest = ClientRequest.from(clientRequest)
+                                        .header(HttpHeaders.AUTHORIZATION, token)
+                                        .build();
+                                return next.exchange(newRequest);
+                            }
+                            return next.exchange(clientRequest);
+                        })
+                        .orElse(next.exchange(clientRequest))
+        );
+    }
+
+    @Bean
+    IamClient iamClient(WebClient.Builder builder) {
+        WebClient webClient = builder
+                .baseUrl("lb://iam-service/iam")
                 .build();
+
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
+                .builderFor(WebClientAdapter.create(webClient)).build();
+
+        return httpServiceProxyFactory.createClient(IamClient.class);
     }
 
     @Bean
@@ -33,14 +69,6 @@ public class WebClientConfiguration {
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 
         return new CorsWebFilter(urlBasedCorsConfigurationSource);
-    }
-
-    @Bean
-    IamClient iamClient(WebClient webClient){
-        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory
-                .builderFor(WebClientAdapter.create(webClient)).build();
-
-        return httpServiceProxyFactory.createClient(IamClient.class);
     }
 
 }
--- patient-service/src/main/java/jungle/patientservice/repo/httpClient/UserClient.java
+++ patient-service/src/main/java/jungle/patientservice/repo/httpClient/UserClient.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "iam-service", url = "${app.user-service.url}", configuration = AuthenticationRequestInterceptor.class)
+@FeignClient(name = "iam-service", path = "/iam",  configuration = AuthenticationRequestInterceptor.class)
 public interface UserClient {
     @GetMapping("/users/{id}")
     RestResponse<UserResponse> getUser(@PathVariable("id") Long id);
