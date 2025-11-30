24df4ba4f70013e705f1f92cf73fbc2864b81bb1
Nguyễn Thành Đạt <datnguyen.coder312@gmail.com>
2025-11-30 16:54:17 +0700

    enable service discovery in NotificationApplication

--- notification-service/src/main/java/ttldd/notification/NotificationApplication.java
+++ notification-service/src/main/java/ttldd/notification/NotificationApplication.java
@@ -2,11 +2,13 @@ package ttldd.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
