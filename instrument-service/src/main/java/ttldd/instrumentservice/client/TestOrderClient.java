78a8fefe4c3a8f90af739e46394a9e5eba2611d7
Nguyễn Thành Đạt <datnguyen.coder312@gmail.com>
2025-11-30 16:40:07 +0700

    update application profiles to use production environment

--- instrument-service/src/main/java/ttldd/instrumentservice/client/TestOrderClient.java
+++ instrument-service/src/main/java/ttldd/instrumentservice/client/TestOrderClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "testOrder-service",configuration = AuthenticationRequestInterceptor.class)
+@FeignClient(name = "testOrder-service", path = "/testorder", configuration = AuthenticationRequestInterceptor.class)
 public interface TestOrderClient {
     @GetMapping("/orders/accessionNumber/testOrder/{accessionNumber}")
     RestResponse<TestOrderDTO> getTestOrdersByAccessionNumber(@PathVariable String accessionNumber);
